7dbeddc2e4f1c8e830f07ef9aad74734d40ad9a7
TIMUR <timur.sinev2000@mail.ru>
2026-03-25 12:47:59 +0300

    tbb11

--- tasks/timur_a_cannon/tbb/src/ops_tbb.cpp
+++ tasks/timur_a_cannon/tbb/src/ops_tbb.cpp
@@ -1,5 +1,7 @@
 #include "timur_a_cannon/tbb/include/ops_tbb.hpp"
+
 #include <tbb/tbb.h>
+
 #include <utility>
 #include <vector>
 
@@ -16,7 +18,9 @@ bool TimurACannonMatrixMultiplicationTBB::ValidationImpl() {
   const auto &mat_a = std::get<1>(input);
   const auto &mat_b = std::get<2>(input);
 
-  if (b_size <= 0 || mat_a.empty() || mat_b.empty()) return false;
+  if (b_size <= 0 || mat_a.empty() || mat_b.empty()) {
+    return false;
+  }
   size_t n = mat_a.size();
   return mat_a[0].size() == n && mat_b.size() == n && (n % static_cast<size_t>(b_size) == 0);
 }
@@ -73,12 +77,16 @@ bool TimurACannonMatrixMultiplicationTBB::RunImpl() {
     if (grid_sz > 1 && step < grid_sz - 1) {
       tbb::parallel_for(0, grid_sz, [&](int i) {
         Matrix first = std::move(bl_a[i][0]);
-        for (int j = 0; j < grid_sz - 1; ++j) bl_a[i][j] = std::move(bl_a[i][j + 1]);
+        for (int j = 0; j < grid_sz - 1; ++j) {
+          bl_a[i][j] = std::move(bl_a[i][j + 1]);
+        }
         bl_a[i][grid_sz - 1] = std::move(first);
       });
       tbb::parallel_for(0, grid_sz, [&](int j) {
         Matrix first = std::move(bl_b[0][j]);
-        for (int i = 0; i < grid_sz - 1; ++i) bl_b[i][j] = std::move(bl_b[i + 1][j]);
+        for (int i = 0; i < grid_sz - 1; ++i) {
+          bl_b[i][j] = std::move(bl_b[i + 1][j]);
+        }
         bl_b[grid_sz - 1][j] = std::move(first);
       });
     }
